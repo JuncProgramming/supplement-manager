fd96d666a5ce51371b2dc38964bd15c161e53f82
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-02-06 09:27:36 +0100

    Add proper navigation menu for mobile devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,106 @@
 import { NavLink } from 'react-router-dom'
-import { PillBottle } from 'lucide-react'
+import { PillBottle, X, Menu, CalendarDays, Package, LayoutDashboard } from 'lucide-react'
+import { useState } from 'react'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
-    <header className="flex h-16 flex-row items-center justify-between border-b border-gray-200 bg-white px-4">
-      <h1 className="flex flex-row items-center gap-1 font-semibold text-blue-600">
-        <PillBottle className="rotate-6" /> Supplement Manager
-      </h1>
-      <nav className="flex gap-4">
-        <NavLink
-          to="/dashboard"
-          className={({ isActive }) =>
-            `font-semibold transition-colors ${
-              isActive ? 'text-blue-800 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'
-            }`
-          }
+    <header className="sticky top-0 z-50 w-full border-b border-gray-200 bg-white">
+      <div className="flex h-16 items-center justify-between px-4">
+        <h1 className="flex flex-row items-center gap-1 font-semibold text-blue-600">
+          <PillBottle className="shrink-0 rotate-6" /> Supplement Manager
+        </h1>
+        <nav className="hidden gap-4 md:flex">
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              `font-semibold transition-colors ${
+                isActive ? 'text-blue-800 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'
+              }`
+            }
+          >
+            Dashboard
+          </NavLink>
+
+          <NavLink
+            to="/inventory"
+            className={({ isActive }) =>
+              `font-semibold transition-colors ${
+                isActive ? 'text-blue-800 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'
+              }`
+            }
+          >
+            Inventory
+          </NavLink>
+
+          <NavLink
+            to="/history"
+            className={({ isActive }) =>
+              `font-semibold transition-colors ${
+                isActive ? 'text-blue-800 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'
+              }`
+            }
+          >
+            History
+          </NavLink>
+        </nav>
+
+        <button
+          className="rounded-lg border-2 border-gray-200 p-2 text-blue-600 md:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
-          Dashboard
-        </NavLink>
-        <NavLink
-          to="/inventory"
-          className={({ isActive }) =>
-            `font-semibold transition-colors ${
-              isActive ? 'text-blue-800 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'
-            }`
-          }
-        >
-          Inventory
-        </NavLink>
-        <NavLink
-          to="/history"
-          className={({ isActive }) =>
-            `font-semibold transition-colors ${
-              isActive ? 'text-blue-800 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'
-            }`
-          }
-        >
-          History
-        </NavLink>
-      </nav>
+          {isMenuOpen ? <X className="shrink-0"></X> : <Menu className="shrink-0"></Menu>}
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <div className="border-t border-gray-100 bg-white px-4 py-4 md:hidden">
+          <nav className="flex flex-col space-y-5">
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) =>
+                `flex gap-2 font-semibold transition-colors ${
+                  isActive
+                    ? 'text-blue-800 hover:text-blue-600'
+                    : 'text-blue-600 hover:text-blue-800'
+                }`
+              }
+            >
+              <LayoutDashboard className="shrink-0"></LayoutDashboard>
+              Dashboard
+            </NavLink>
+
+            <NavLink
+              to="/inventory"
+              className={({ isActive }) =>
+                `flex gap-2 font-semibold transition-colors ${
+                  isActive
+                    ? 'text-blue-800 hover:text-blue-600'
+                    : 'text-blue-600 hover:text-blue-800'
+                }`
+              }
+            >
+              <Package className="shrink-0"></Package>
+              Inventory
+            </NavLink>
+
+            <NavLink
+              to="/history"
+              className={({ isActive }) =>
+                `flex gap-2 font-semibold transition-colors ${
+                  isActive
+                    ? 'text-blue-800 hover:text-blue-600'
+                    : 'text-blue-600 hover:text-blue-800'
+                }`
+              }
+            >
+              <CalendarDays className="shrink-0"></CalendarDays>
+              History
+            </NavLink>
+          </nav>
+        </div>
+      )}
     </header>
   )
 }
