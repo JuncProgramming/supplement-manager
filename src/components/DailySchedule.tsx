dd1e12bbe97b88d00cc4d827132e0e6cf2b489b7
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-03-01 15:56:09 +0100

    Add a DailySchedule component for use in the dashboard page

---
+++ src/components/DailySchedule.tsx
@@ -0,0 +1,58 @@
+import { TIMES_OF_DAY, TIME_OF_DAY_LABELS } from '@/types'
+import type { Supplement, TimeOfDayType } from '@/types'
+import { Fragment } from 'react/jsx-runtime'
+
+const DailySchedule = ({
+  supplements,
+  onCheckboxToggle,
+  checkedBoxes
+}: {
+  supplements: Supplement[]
+  onCheckboxToggle: (supplement: Supplement, timeOfDay: TimeOfDayType) => void
+  checkedBoxes: Record<string, boolean>
+}) => {
+  return (
+    <div className="grid w-full grid-cols-[2fr_repeat(6,1fr)] rounded-lg border-2 border-gray-200 bg-white px-4 py-2 pb-4 shadow-md">
+      <div className="p-2"></div>
+      {Object.values(TIMES_OF_DAY).map((timeOfDay) => (
+        <div
+          key={timeOfDay}
+          className="flex items-center justify-center border-gray-200 p-2 text-center font-semibold text-gray-800"
+        >
+          {TIME_OF_DAY_LABELS[timeOfDay]}
+        </div>
+      ))}
+
+      {supplements.map((supplement) => (
+        <Fragment key={supplement.id}>
+          <div className="flex min-w-0 items-center border-t-2 border-gray-200 p-2 font-medium wrap-anywhere">
+            {supplement.name}
+          </div>
+          {Object.values(TIMES_OF_DAY).map((timeOfDay) => {
+            const isScheduled = supplement.timesOfDay.includes(timeOfDay)
+
+            return (
+              <div
+                key={`${supplement.id}-${timeOfDay}`}
+                className="flex items-center justify-center border-t-2 border-gray-200 p-2"
+              >
+                <input
+                  onChange={() => onCheckboxToggle(supplement, timeOfDay)}
+                  checked={
+                    checkedBoxes[`${supplement.id}-${timeOfDay}`] ??
+                    false /* setting false as a fallback, because react complains with an 'uncontrolled input' error */
+                  }
+                  disabled={!isScheduled}
+                  className="cursor-pointer rounded transition-colors focus:ring-0 focus:ring-offset-0 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 disabled:opacity-30"
+                  type="checkbox"
+                />
+              </div>
+            )
+          })}
+        </Fragment>
+      ))}
+    </div>
+  )
+}
+
+export default DailySchedule
